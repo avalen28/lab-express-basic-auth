83014b5449e4c6df9f67fc263820637705306cfd
Alberto Valenzuela <avalen28@gmail.com>
2023-01-20 12:46:25 +0100

    adds login-

--- app.js
+++ app.js
@@ -15,14 +15,36 @@ const hbs = require("hbs");
 
 const app = express();
 
+//user session express & mongo:-----------------------------------------
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require("./config")(app);
+
+app.set("trust proxy", 1);
+app.use(
+  session({
+    name: "auth-app",
+    secret: process.env.SESSION_SECRET, // en .env se cambia por el value que le haya dado
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      maxAge: 2592000000, // 30 days in milliseconds
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL, //también definido en .env
+    }),
+  })
+);
 
 // default value for title local
 const projectName = "lab-express-basic-auth";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
-
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,12 @@ const saltRounds = 10;
 
 const User = require("../models/User.model");
 
+// rutas de sign up
 router.get("/", (req, res, next) => {
   res.render("auth/signup");
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   const { username, password } = req.body;
   // Esto seria necesario para avisar de un fallo
   //   if (!username || !password) {
@@ -32,9 +33,51 @@ router.post("/", async (req, res, next) => {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
     await User.create({ username, hashedPassword });
-    res.render("auth/signup", { signupDone: "user created" }); // de momento lo dejo así para posterior saber a donde redirigir
+    res.redirect("/auth/login");
   } catch (error) {
     console.log(error);
   }
 });
+
+// rutas de login
+router.get("/login", (req, res, next) => res.render("auth/login"));
+
+router.post("/login", async (req, res, next) => {
+  console.log("entro??????");
+  const { name, password } = req.body;
+  if (!name || !password) {
+    res.render("auth/login", {
+      error: "Introduce a correct name and password to log In",
+    });
+    return;
+  }
+  try {
+    const userInDB = await User.findOne({ username: name }); // encontrar usuario y traer toda la info
+    console.log("info: user in DB is", userInDB);
+    if (!userInDB) {
+      console.log("user is not in db");
+      res.render("auth/login", { error: `The user ${name} doesn't exist` });
+      return;
+    } else {
+      console.log("user is in db");
+      const passwordMatch = await bcrypt.compare(
+        password,
+        userInDB.hashedPassword
+      );
+      if (passwordMatch) {
+        console.log("passwords match");
+        req.session.currentUser = userInDB; //por si queremos añadir datos del usuario. con esto hago "link" a todas las req.
+        const { currentUser } = req.session;
+        res.render("welcome", currentUser);
+      } else {
+        console.log("passwords don't match", password, userInDB.hashedPassword);
+        res.render("auth/login", { error: "incorrect password" });
+        return;
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
